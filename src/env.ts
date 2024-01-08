78c21b5dbf73ec269584f613b63e20e32411fc19
Aditya Deshmukh <a.deshmukh@hella.com>
2024-01-08 20:21:49 +0530

    FEAT: adding app's secret key in env, enforcing required env variables

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,15 @@
+if (!process.env.MONGO_URL) {
+    throw Error('MONGO_URL not found in environment!');
+} 
+if (!process.env.SECRET_KEY) {
+    throw Error('SECRET_KEY not found in environment!');
+}
+
+// Optional
 export const PORT: number = Number(process.env.PORT) || 8080;
-export const MONGO_URL: string = process.env.MONGO_URL;
 export const DEBUG_MODE: boolean = process.env.DEBUG_MODE == '1';
 export const LOG_LEVEL: string = process.env.LOG_LEVEL || 'debug';
+
+// Required
+export const MONGO_URL: string = process.env.MONGO_URL;
+export const SECRET_KEY: string = process.env.SECRET_KEY;
