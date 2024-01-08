eb8a8c702585727b72be677c2bf3766c53968b80
Aditya Deshmukh <a.deshmukh@hella.com>
2024-01-08 20:15:19 +0530

    FIX: returning value from non void function

--- src/models/users.ts
+++ src/models/users.ts
@@ -17,7 +17,7 @@ export const getUsers = () => UserModel.find();
 export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 
 export const getUserBySessionToken = (sessionToken: string) => {
-    UserModel.findOne({
+    return UserModel.findOne({
         "authentication.sessionToken": sessionToken,
     });
 }
