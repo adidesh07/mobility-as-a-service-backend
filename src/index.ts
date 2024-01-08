f5689db47a0abce7961916a00020a1cf43f86b44
Aditya Deshmukh <a.deshmukh@hella.com>
2024-01-08 19:50:58 +0530

    FEAT: adding mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import compression from "compression";
-import { PORT } from "./env";
+import mongoose from "mongoose";
+import { PORT, MONGO_URL } from "./env";
 
 const app = express();
 
@@ -11,4 +12,12 @@ app.use(cors({ credentials: true }));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL)
+    .then(() => {
+        console.log('Successfully connected to database')
+        mongoose.connection.on('error', (error: Error) => console.log(error));
+        app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    })
+    .catch((err) => console.log('Error occurred while connecting to database\n', err));
